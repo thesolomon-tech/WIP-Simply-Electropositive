8e822965e940e2fc196dc4f0341ffaaa0c7ed618
Solomon Khoury <solomon@thesolomon.tech>
2024-01-17 12:02:52 +1100

    Initial

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => createWindow())
+//above code creates a new window
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+//above code is boilerplate for when app windows close on different system types.
